6b4a610e43cb8804185aeaf23d43e53c9a2844bf
Timothy Moore <mtimothy984@gmail.com>
2020-03-06 19:41:09 -0800

    libpq-dev changed to postgresql-devel

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo yum -y install python3 make glibc-devel gcc patch python3-devel libcurl4-gnutls-dev libxml2-dev libssl-dev postgresql-common libpq-dev postgresql-client
+sudo yum -y install python3 make glibc-devel gcc patch python3-devel libcurl4-gnutls-dev libxml2-dev libssl-dev postgresql-common postgresql-devel postgresql-client
 sudo python3 -m pip install --upgrade pip
 rm -rf /webapps/reddit-proxy/src
 rm -rf /webapps/reddit-proxy/scripts
