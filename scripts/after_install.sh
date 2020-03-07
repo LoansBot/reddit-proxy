e267ddae470561f26c362e4cbb47125fc08eab6a
Timothy Moore <mtimothy984@gmail.com>
2020-03-06 20:18:42 -0800

    Fix supervisor not working in sudo mode

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 sudo python3 -m pip install -r /webapps/reddit-proxy/requirements.txt
 sudo python3 -m pip install -r /webapps/reddit-proxy/logging-requirements.txt
-if supervisorctl reread; then
+if /usr/local/bin/supervisorctl reread; then
     echo "Supervisor reread succeeded"
 else
     echo "Starting up supervisor"
-    supervisord -c /webapps/reddit-proxy/cfg/supervisor.conf || :
+    /usr/local/bin/supervisord -c /webapps/reddit-proxy/cfg/supervisor.conf || :
 fi
