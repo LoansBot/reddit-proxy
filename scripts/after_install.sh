950a220e069df845e90371239ad4284d991643cb
Timothy Moore <mtimothy984@gmail.com>
2020-03-06 20:22:46 -0800

    Actually pass envvars to supervisor

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 sudo python3 -m pip install -r /webapps/reddit-proxy/requirements.txt
 sudo python3 -m pip install -r /webapps/reddit-proxy/logging-requirements.txt
-if /usr/local/bin/supervisorctl reread; then
+if sudo /usr/local/bin/supervisorctl reread; then
     echo "Supervisor reread succeeded"
 else
     echo "Starting up supervisor"
-    /usr/local/bin/supervisord -c /webapps/reddit-proxy/cfg/supervisor.conf || :
+    source /home/ec2-user/secrets.sh
+    sudo -E /usr/local/bin/supervisord -c /webapps/reddit-proxy/cfg/supervisor.conf || :
 fi
