704e1e65a3343f8ca7cfe996b0ff721fe112e148
Timothy Moore <mtimothy984@gmail.com>
2020-05-04 07:02:38 -0700

    Add modlog handler

---
+++ src/endpoints/modlog.py
@@ -0,0 +1,42 @@
+"""Provides mappings to endpoints related to the moderator log"""
+import requests
+
+
+class ModLogEndpoint:
+    def __init__(self, default_headers):
+        self.name = 'modlog'
+        self.default_headers = default_headers
+
+    def make_request(self, subreddit, limit, after, before, auth):
+        """Fetch up to limit items either after after, before before, or from
+        the beginning of time for the given subreddit. Only one of after and
+        before should be set.
+
+        :param subreddit: The subreddit (or multiple joined by +) to get the
+            moderator actions from
+        :param limit: The maximum number of items to return (default 25, max
+            100)
+        :param after: The fullname to start the listing at, or None if either
+            before is set or you want to start at the beginning
+        :param before: The fullname to end the listing at, or None if either
+            after is set or you want to start at the beginning
+        :param auth: The authorization method
+        """
+        data = {}
+        if limit is not None:
+            data['limit'] = limit
+        if after is not None:
+            data['after'] = after
+        if before is not None:
+            data['before'] = before
+        return requests.get(
+            f'https://oauth.reddit.com/r/{subreddit}/log.json',
+            headers={**self.default_headers, **auth.get_auth_headers()},
+            data=data
+        )
+
+
+def register_endpoints(arr, headers):
+    arr += [
+        ModLogEndpoint(headers)
+    ]
---
+++ tests/integration/test_modlog.py
@@ -0,0 +1,78 @@
+"""Tests fetching some stuff from a modlog"""
+import unittest
+import os
+import pika
+import json
+import time
+import helper
+
+
+PIKA_PARAMETERS = pika.ConnectionParameters(
+    os.environ['AMQP_HOST'],
+    int(os.environ['AMQP_PORT']),
+    os.environ['AMQP_VHOST'],
+    pika.PlainCredentials(
+        os.environ['AMQP_USERNAME'], os.environ['AMQP_PASSWORD']
+    )
+)
+
+
+QUEUE = os.environ['AMQP_QUEUE']
+
+
+RESPONSE_QUEUE = 'ping_resp_queue'
+
+
+class PingTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        amqp = pika.BlockingConnection(PIKA_PARAMETERS)
+        channel = amqp.channel()
+        channel.queue_declare(QUEUE)
+        channel.queue_declare(RESPONSE_QUEUE)
+        cls.channel = channel
+        cls.amqp = amqp
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.channel.close()
+        cls.amqp.close()
+
+    def test_modlog(self):
+        self.channel.basic_publish(
+            '',
+            QUEUE,
+            json.dumps({
+                'type': 'modlog',
+                'response_queue': RESPONSE_QUEUE,
+                'uuid': 'modlog-uuid',
+                'version_utc_seconds': 1,
+                'sent_at': time.time(),
+                'args': {
+                    'subreddits': [os.environ['REDDIT_MOD_SUBREDDIT']],
+                    'limit': 7
+                }
+            })
+        )
+        body = helper.fetch_one(self, RESPONSE_QUEUE)
+
+        self.assertEqual(body.get('type'), 'copy')
+        self.assertIsInstance(body.get('info'), 'dict')
+
+        info = body['info']
+        self.assertIsInstance(info.get('after'), (type(None), str))
+        self.assertIsInstance(info.get('actions'), list)
+        for act in info['actions']:
+            self.assertIsInstance(act.get('target_fullname'), (type(None), str))
+            self.assertIsInstance(act.get('target_author'), (type(None), str))
+            self.assertIsInstance(act.get('mod'), str)
+            self.assertIsInstance(act.get('action'), str)
+            self.assertIsInstance(act.get('details'), (type(None), str))
+            self.assertIsInstance(act.get('subreddit'), str)
+            self.assertIsInstance(act.get('created_utc'), float)
+
+        self.assertLessEqual(len(info['actions']), 7)
+
+
+if __name__ == '__main__':
+    unittest.main()
