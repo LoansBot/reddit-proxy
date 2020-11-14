83150df1c77c917d2f278bd022ca223a1ad9eb9b
Timothy Moore <mtimothy984@gmail.com>
2020-11-14 07:28:39 -0800

    Add flair link endpoint (#36)
    * Add flair link endpoint
    
    * Give more time for reddit to respond in tests

--- tests/integration/helper.py
+++ tests/integration/helper.py
@@ -4,7 +4,7 @@ import json
 
 def fetch_one(self, queue):
     """Fetch one message from the given queue"""
-    for method_frame, properties, body_bytes in self.channel.consume(queue, inactivity_timeout=5):
+    for method_frame, properties, body_bytes in self.channel.consume(queue, inactivity_timeout=20):
         self.assertIsNotNone(method_frame)
         self.channel.basic_ack(method_frame.delivery_tag)
         return json.loads(body_bytes.decode('utf-8'))
