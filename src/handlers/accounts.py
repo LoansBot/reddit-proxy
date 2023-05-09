3d79ca9f8f1399f0a1cfeb6ee4177acfe5362b64
Timothy Moore <mtimothy984@gmail.com>
2023-05-09 09:33:44 -0700

    Add link and comment karma breakdown (#45)

--- src/handlers/accounts.py
+++ src/handlers/accounts.py
@@ -12,22 +12,29 @@ class UserShowHandler:
 
     {
         "cumulative_karma": int,
+        "link_karma": int,
+        "comment_karma": int,
         "created_at_utc_seconds": float
     }
     """
+
     def __init__(self):
-        self.name = 'show_user'
+        self.name = "show_user"
         self.requires_delay = True
 
     def handle(self, reddit, auth, data):
-        result = reddit.show_user(auth, data['username'])
+        result = reddit.show_user(auth, data["username"])
         if result.status_code > 299:
             return result.status_code, None
 
         body = result.json()
         return result.status_code, {
-            'cumulative_karma': body['data']['link_karma'] + body['data']['comment_karma'],
-            'created_at_utc_seconds': float(body['data']['created_utc'])
+            "cumulative_karma": (
+                body["data"]["link_karma"] + body["data"]["comment_karma"]
+            ),
+            "link_karma": body["data"]["link_karma"],
+            "comment_karma": body["data"]["comment_karma"],
+            "created_at_utc_seconds": float(body["data"]["created_utc"]),
         }
 
 
@@ -45,25 +52,24 @@ class UserIsModeratorHandler:
         "moderator": bool
     }
     """
+
     def __init__(self):
-        self.name = 'user_is_moderator'
+        self.name = "user_is_moderator"
         self.requires_delay = True
 
     def handle(self, reddit, auth, data):
-        result = reddit.user_is_moderator(auth, data['subreddit'], data['username'])
+        result = reddit.user_is_moderator(auth, data["subreddit"], data["username"])
         if result.status_code > 299:
             return result.status_code, None
 
         body = result.json()
         rel_exists = any(
             True
-            for ele in body['data']['children']
-            if ele['name'].lower() == data['username'].lower()
+            for ele in body["data"]["children"]
+            if ele["name"].lower() == data["username"].lower()
         )
 
-        return result.status_code, {
-            'moderator': rel_exists
-        }
+        return result.status_code, {"moderator": rel_exists}
 
 
 class UserIsApprovedHandler:
@@ -80,25 +86,24 @@ class UserIsApprovedHandler:
         "approved": bool
     }
     """
+
     def __init__(self):
-        self.name = 'user_is_approved'
+        self.name = "user_is_approved"
         self.requires_delay = True
 
     def handle(self, reddit, auth, data):
-        result = reddit.user_is_approved(auth, data['subreddit'], data['username'])
+        result = reddit.user_is_approved(auth, data["subreddit"], data["username"])
         if result.status_code > 299:
             return result.status_code, None
 
         body = result.json()
         rel_exists = any(
             True
-            for ele in body['data']['children']
-            if ele['name'].lower() == data['username'].lower()
+            for ele in body["data"]["children"]
+            if ele["name"].lower() == data["username"].lower()
         )
 
-        return result.status_code, {
-            'approved': rel_exists
-        }
+        return result.status_code, {"approved": rel_exists}
 
 
 class UserIsBannedHandler:
@@ -115,25 +120,24 @@ class UserIsBannedHandler:
         "banned": bool
     }
     """
+
     def __init__(self):
-        self.name = 'user_is_banned'
+        self.name = "user_is_banned"
         self.requires_delay = True
 
     def handle(self, reddit, auth, data):
-        result = reddit.user_is_banned(auth, data['subreddit'], data['username'])
+        result = reddit.user_is_banned(auth, data["subreddit"], data["username"])
         if result.status_code > 299:
             return result.status_code, None
 
         body = result.json()
         rel_exists = any(
             True
-            for ele in body['data']['children']
-            if ele['name'].lower() == data['username'].lower()
+            for ele in body["data"]["children"]
+            if ele["name"].lower() == data["username"].lower()
         )
 
-        return result.status_code, {
-            'banned': rel_exists
-        }
+        return result.status_code, {"banned": rel_exists}
 
 
 def register_handlers(handlers):
@@ -141,5 +145,5 @@ def register_handlers(handlers):
         UserShowHandler(),
         UserIsModeratorHandler(),
         UserIsApprovedHandler(),
-        UserIsBannedHandler()
+        UserIsBannedHandler(),
     ]
